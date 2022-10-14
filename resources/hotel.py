b186110b20c54499e0d0b072e334b8ce49aeb640
matheusalmeida01 <matheus.almeida@think.br.com>
2022-10-14 18:16:53 -0300

    DELETE hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -47,25 +48,16 @@ class Hotel(Resource):
         return {'message' : 'hotel not found'}, 404
     
     def post(self, hotel_id):
-        
         dados = Hotel.argumentos.parse_args()
-        
-        novo_hotel = {
-            'hotel_id' : hotel_id,
-            'nome' : dados['nome'],
-            'estrelas' : dados['estrelas'],
-            'diaria' : dados['diaria'],
-            'cidade' : dados['cidade']
-        }
-        
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
     
     def put(self, hotel_id):
-        
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = { 'hotel_id' : hotel_id, **dados }
-        
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
@@ -74,4 +66,6 @@ class Hotel(Resource):
         return novo_hotel, 201
     
     def delete(self, hotel_id):
-        pass
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message' : 'Hotel deleted'}
