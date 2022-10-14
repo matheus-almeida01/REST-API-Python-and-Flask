d7438500a76c203cd6814ed11f49c50bd80703c2
matheusalmeida01 <matheus.almeida@think.br.com>
2022-10-14 17:51:25 -0300

    POST hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -16,7 +16,7 @@ hoteis = [
         'cidade' : 'Santa Catrina'
     },
     {
-        'hotel_id' : 'charli',
+        'hotel_id' : 'charlie',
         'nome' : 'Charlie Hotel',
         'estrelas' : 3.9,
         'diaria' : 320.20,
@@ -29,13 +29,37 @@ class Hoteis(Resource):
         return {'hoteis': hoteis}
     
 class Hotel(Resource):
-    def get(self, hotel_id):
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return {'message' : 'hotel not found'}, 404
+    
     def post(self, hotel_id):
-        pass
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas'),
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+        
+        dados = argumentos.parse_args()
+        
+        novo_hotel = {
+            'hotel_id' : hotel_id,
+            'nome' : dados['nome'],
+            'estrelas' : dados['estrelas'],
+            'diaria' : dados['diaria'],
+            'cidade' : dados['cidade']
+        }
+        
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+    
     def put(self, hotel_id):
         pass
     def delete(self, hotel_id):
