f4a5d37eeb26869d2fe786e20f8e5626a5fc4597
matheusalmeida01 <matheus.almeida@think.br.com>
2022-10-17 16:55:37 -0300

    user register and login

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User
+from resources.usuario import User, UserLogin, UserRegister
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///banco.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = "DontTellAnyone"
 api = Api(app)
+jwt = JWTManager(app)
 
 @app.before_first_request
 def cria_banco():
@@ -15,6 +18,8 @@ def cria_banco():
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(User, 'usuarios/<int:user_id>')
+api.add_resource(UserRegister, '/cadastro')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == 'main':
     from sql_alchemy import banco
